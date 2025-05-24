bd16635b7f3623a33784453ac19f23368dc336e6
FranVlahovic <franvlahovic@proton.me>
2025-05-24 19:14:53 +0200

    feat: Add DefaultProfile, ErrorPage, Popeye, Profile, Spinach and Routes JSX files. Added JSX files to navigate between using the react router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function App(){
-    return ();
+    return (
+        <div>
+            <h1>Hello from the main page of the app!</h1>
+            <p>Here are some examples of links to other pages</p>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="profile">Profile page</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 }
